da46cb3525a8ccd06f7b1fb4ed3c40f72dafad20
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-23 20:31:56 -0300

    curso js da origamid

---
+++ variaveis/script.js
@@ -0,0 +1,44 @@
+/*var nome = "Thiago";
+var idade = 33;
+var possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobreNome = "Miziara",
+    cidade = "Uberlândia";
+
+console.log(sobreNome, cidade);
+
+var precoAplicativo;
+
+console.log(precoAplicativo);
+
+var time = "Flamengo";
+time = "Mengão";
+
+let time2 = 'Vasco';
+time2 = " Vasco  = Serie B";
+device-width
+const time3 = "Fluminense";
+*/
+// Exercicios do Capitulo de Váriaveis
+
+let nome = 'Thiago';
+let idade = 33;
+
+let comidaFavorita;
+comidaFavorita = "Pizza";
+
+let cidade,
+    lsobreNome,
+    sexo,
+    casado,
+    qtoFilhos;
+
+console.log(nome, idade, comidaFavorita);
