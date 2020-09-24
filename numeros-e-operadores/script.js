c6ede06c7ba977ccc422b64732247dedde7a8a0c
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-24 18:00:36 -0300

    operadores

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,32 @@
+var total = 10 + 5 + 5;
+var divisao = 200 / 5;
+var modulo = 3897879587 % 3;
+
+
+console.log(total);
+console.log(divisao);
+console.log(modulo);
+
+var testeNan = "isso  é 100" / 2;
+console.log(isNaN(testeNan));
+
+
+var soma = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+
+console.log(soma);
+
+var incremento = 5;
+console.log(incremento++);
+console.log(incremento);
+
+var decremento = 5;
+console.log(decremento--);
+console.log(decremento);
+
+var idade = "33";
+var somaIdade = 5;
+console.log(+idade + somaIdade);
+
+
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
---
+++ numeros-e-operadores/scriptAula.js
@@ -0,0 +1,33 @@
+//Qula o resultado da seguinte  expressão? = 35
+
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+// Crie duas espressoes que renonem NaN
+
+var testeNan = '5s ';
+var testeNan2 = 5;
+console.log(testeNan / testeNan2);
+
+var NaN0 = 1;
+var NaN1 = "isso nao vai funcionar  50 ";
+console.log(NaN0 * NaN1);
+
+//somar string "200" com o número 50 e retornar 250
+var soma = '200';
+var soma1 = 50;
+console.log(+soma + soma1);
+
+// increment o número 5 e retorne seu valor incrementado
+
+var incremento = 5;
+console.log(incremento++);
+console.log(incremento);
+
+// como dividir o peso por 2?
+
+var numero = +"80" / 2;
+var unidade = 'Kg';
+var peso = numero + unidade;
+
+console.log(peso);
