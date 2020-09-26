606280113340480c5b914deba2685895c194e4d7
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-26 19:35:48 -0300

    tudo e objeto

---
+++ tudoEObjeto/script.js
@@ -0,0 +1,14 @@
+var nome = "Thiago"
+nome.toLocaleUpperCase();
+nome.fixed();
+nome.length;
+console.log(nome.length, nome.fixed(), nome.toLocaleUpperCase());
+
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', function() {
+
+});
+
+//btn.appendChild();
+//btn.before();
+//btn.addEventListener();
