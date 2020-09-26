67a0037b68bf02fb7105fd91e1129254e18b5375
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-26 20:28:32 -0300

    arrays loops

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,18 @@
+var videoGames = ['Switch', 'Ps4', 'Xbox'];
+
+//videoGames.pop();
+var ultimoItem = videoGames.pop('3DS')
+
+videoGames.push("3DS");
+
+videoGames.length;
+
+for (var numero = 0; numero <= 10; numero++) {
+    console.log(numero);
+}
+
+var i = 0;
+while (i <= 10) {
+    console.log(i)
+    i++;
+}
---
+++ arrays-e-loops/scriptAula.js
@@ -0,0 +1,10 @@
+var nome = 'Thiago';
+
+var nomeMinusculo = nome.toLocaleLowerCase();
+var nomeMaiusculo = nome.toLocaleUpperCase();
+
+console.log(nome, nomeMinusculo, nomeMaiusculo);
+
+var altura = 1.8;
+
+var btn = document.querySelector('.btn');
