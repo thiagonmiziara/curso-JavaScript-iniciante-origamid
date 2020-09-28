561dcf10200003bd000690383a666c56f04e0169
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-28 16:01:12 -0300

    arrays e loops

--- arrays-e-loops/script.js
+++ arrays-e-loops/script.js
@@ -1,18 +1,18 @@
-var videoGames = ['Switch', 'Ps4', 'Xbox'];
-
-//videoGames.pop();
-var ultimoItem = videoGames.pop('3DS')
-
-videoGames.push("3DS");
-
-videoGames.length;
-
-for (var numero = 0; numero <= 10; numero++) {
-    console.log(numero);
+let brasilCampeao = ['1959', '1962', '1970', '1994', '2002'];
+//console.log(anos);
+for (let i = 0; i < brasilCampeao.length; i++) {
+    console.log(`O Brasil ganhou a copa de ${brasilCampeao[i]} `)
 }
 
-var i = 0;
-while (i <= 10) {
-    console.log(i)
-    i++;
+let frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+for (var i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+    if (frutas[i] === 'Pera') {
+        break;
+
+    }
 }
+
+let ultimaFruta = frutas[frutas.length - 1];
+console.log(ultimaFruta)
--- arrays-e-loops/scriptAula.js
+++ arrays-e-loops/scriptAula.js
@@ -1,10 +1,46 @@
-var nome = 'Thiago';
+var videoGames = ['Switch', 'Ps4', 'Xbox', '3DS'];
 
-var nomeMinusculo = nome.toLocaleLowerCase();
-var nomeMaiusculo = nome.toLocaleUpperCase();
+for (var i = 0; i < videoGames.length; i++) {
+    console.log(videoGames[i]);
+    if (videoGames[i] === 'Ps4') {
+        break;
+    }
+}
 
-console.log(nome, nomeMinusculo, nomeMaiusculo);
+var videoGames1 = ['Switch', 'Ps4', 'Xbox', '3DS'];
+videoGames1.forEach(function(games) {
+    console.log(games);
+});
 
-var altura = 1.8;
+var frutas = ['Banana', 'Pera', 'Maçã', 'Abacaxi', 'Uva'];
+frutas.forEach(function(fruta, index, array) {
+    frutas.pop();
+    console.log(fruta, index, array);
+});
+/*
+videoGames.pop();
+var ultimoItem = videoGames.pop('3DS')
 
-var btn = document.querySelector('.btn');
+videoGames.push("3DS");
+
+videoGames.length;
+
+*/
+
+for (var numero = 0; numero <= 5; numero++) {
+    console.log(numero);
+}
+
+var i = 0;
+while (i <= 3) {
+    console.log(i)
+    i++;
+}
+
+
+var numero = 0;
+var maximo = 50;
+for (; numero <= 50;) {
+    console.log(numero);
+    numero++;
+}
