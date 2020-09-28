26e78d97e035e0ebeeb6c66d77902b7a332fd359
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-28 17:38:19 -0300

    escopo

---
+++ escopo/script.js
@@ -0,0 +1,29 @@
+{
+    var cor = 'Preto';
+    const marca = 'Fiat';
+    let portas = 4;
+    console.log(cor, marca, portas);
+}
+
+
+const dois = 2;
+
+function somarDois(x) {
+    return x + dois;
+}
+
+function dividirDois(x) {
+    return x / dois;
+}
+
+console.log(somarDois(4));
+console.log(dividirDois(6));
+
+
+const numero = 50;
+
+for (let numero = 0; numero <= 10; numero++) {
+    console.log(numero);
+}
+const total = numero * 10;
+console.log(total);
---
+++ escopo/scriptAula.js
@@ -0,0 +1,36 @@
+var carro = 'Fusca';
+
+function mostraCarro() {
+    console.log(carro);
+}
+mostraCarro();
+console.log(carro);
+
+
+if (true) {
+    const mes = 'Dezembro'
+    console.log(mes);
+}
+
+//console.log(mes);
+{
+    var carro2 = 'Brasilia';
+    const ano = 1987;
+}
+console.log(carro2);
+//console.log(ano);
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Número ${i}`)
+}
+
+const semana = 'Sexta';
+//semana = 'Quinta';
+console.log(semana);
+
+const data = {
+    ano: 2020,
+    mes: 'Dezembro',
+}
+data.dia = 25;
+console.log(data)
